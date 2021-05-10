e011cb2aa679c816babdee5828e6e26b486e9a75
AlbertoJPS <albertojps.psn@outlook.com>
2021-05-10 09:53:23 -0300

    add exc 06

--- Aula_05/Program.cs
+++ Aula_05/Program.cs
@@ -6,7 +6,46 @@ namespace Aula_05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //exc 06
+
+            //string[] nomes = new string[4];
+            //string[] cpf = new string[4];
+            //int[] idade = new int[4];
+
+            //while (true)
+            //{
+
+
+            //    for (int i = 0; i < nomes.Length; i++)
+            //    {
+            //        Console.WriteLine();
+            //        Console.WriteLine("Aluno : " + (i + 1));
+            //        Console.WriteLine("Nome : " + nomes[i]);
+            //        Console.WriteLine("Cpf : " + cpf[i]);
+            //        Console.WriteLine("Idade : " + idade[i]);
+            //    }
+            //    Console.WriteLine();
+            //    Console.WriteLine("Qual aluno deseja preecher");
+            //    int escolha = Convert.ToInt32(Console.ReadLine())/*-1*/;
+            //    // escolha--;
+            //    Console.WriteLine("Insira o nome:");
+            //    nomes[escolha - 1] = Console.ReadLine();
+            //    Console.WriteLine("Insira o cpf:");
+            //    cpf[escolha - 1] = Console.ReadLine();
+            //    Console.WriteLine("Insira a idade:");
+            //    idade[escolha - 1] = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine();
+            //    Console.WriteLine("Deseja sair?\n1 - sim | 2 - não");
+            //    escolha = Convert.ToInt32(Console.ReadLine());
+            //    if (escolha == 1)
+            //    {
+            //        break;
+            //    }
+
+            //}
+
+
+            //exc 
         }
     }
 }
