bb903ec5c2f2840745e15df6b63ff8ac253cc3f1
AlbertoJPS <albertojps.psn@outlook.com>
2021-05-13 08:58:04 -0300

    exc 07

--- Aula_05/Program.cs
+++ Aula_05/Program.cs
@@ -68,77 +68,113 @@ namespace Aula_05
 
             //exc 002
 
-            string[] produtos = new string[5];
-            int[] compra = new int[10];
-          
+            //    string[] produtos = new string[5];
+            //    int[] compra = new int[10];
 
-        Console.WriteLine("Produtos");
-            Console.WriteLine();
-            Console.WriteLine("Cuca    - 10 Reais");
-            Console.WriteLine("Pão     - 1  Real");
-            Console.WriteLine("Pastel  - 5  Reais");
-            Console.WriteLine("Coxinha - 4  Reais");
-            Console.WriteLine("Bolo    - 35 Reais");
-            Console.WriteLine();
-            Console.WriteLine("*c - Para consultar do valor total da compra*");
-            Console.WriteLine("*s - Para finalizar compra*");
-            Console.WriteLine("vvvvvvvvvvvvvvvv");
-            Console.WriteLine();
-            
 
-            for (int i = 0; i < compra.Length; i++)
+            //Console.WriteLine("Produtos");
+            //    Console.WriteLine();
+            //    Console.WriteLine("Cuca    - 10 Reais");
+            //    Console.WriteLine("Pão     - 1  Real");
+            //    Console.WriteLine("Pastel  - 5  Reais");
+            //    Console.WriteLine("Coxinha - 4  Reais");
+            //    Console.WriteLine("Bolo    - 35 Reais");
+            //    Console.WriteLine();
+            //    Console.WriteLine("*c - Para consultar do valor total da compra*");
+            //    Console.WriteLine("*s - Para finalizar compra*");
+            //    Console.WriteLine("vvvvvvvvvvvvvvvv");
+            //    Console.WriteLine();
+
+
+            //    for (int i = 0; i < compra.Length; i++)
+            //    {
+
+            //        Console.WriteLine();
+            //        Console.WriteLine("Carrinho de Compras");
+            //        for (int b = 0; b < compra.Length; b++)
+            //        {
+            //            produtos[i] = "--";
+            //            if (produtos[i] != "--")
+            //            { 
+            //            Console.WriteLine(produtos[i]);
+            //            }
+            //        }
+            //        Console.WriteLine("Qual produto deseja comprar?");
+            //        string escolha = Console.ReadLine();
+            //        escolha = escolha.ToLower();
+            //        switch(escolha)
+            //        {
+            //            case "cuca":
+            //                compra[i] += 10;
+            //                produtos[i] = "Cuca    - 10 Reais";
+            //                break;
+            //            case "pao":
+            //            case "pão":
+            //                compra[i] += 1;
+            //                produtos[i] = "Pão     - 1  Real";
+            //                break;
+            //            case "pastel":
+            //                compra[i] += 5;
+            //                produtos[i] = "Pastel  - 5  Reais";
+            //                break;
+            //            case "coxinha":
+            //                compra[i] += 4;
+            //                produtos[i] = "Coxinha - 4  Reais";
+            //                break;
+            //            case "bolo":
+            //                compra[i] += 35;
+            //                produtos[i] = "Bolo    - 35 Reais";
+            //                break;
+            //            case "c":
+            //                Console.WriteLine(compra[i]);
+            //                i--;
+            //                break;
+            //            case "s":
+
+            //                break;
+            //            default:
+            //                i--;
+            //                Console.WriteLine("Produto não encontrado");
+            //                break;
+            //       }
+
+            //    }
+
+            // exc 07
+
+            int[][] matriz = new int[3][];
+            int valorFinal = 0;
+
+            for (int i = 0; i < matriz.Length; i ++)
             {
-                
-                Console.WriteLine();
-                Console.WriteLine("Carrinho de Compras");
-                for (int b = 0; b < compra.Length; b++)
-                {
-                    produtos[i] = "--";
-                    if (produtos[i] != "--")
-                    { 
-                    Console.WriteLine(produtos[i]);
-                    }
-                }
-                Console.WriteLine("Qual produto deseja comprar?");
-                string escolha = Console.ReadLine();
-                escolha = escolha.ToLower();
-                switch(escolha)
-                {
-                    case "cuca":
-                        compra[i] += 10;
-                        produtos[i] = "Cuca    - 10 Reais";
-                        break;
-                    case "pao":
-                    case "pão":
-                        compra[i] += 1;
-                        produtos[i] = "Pão     - 1  Real";
-                        break;
-                    case "pastel":
-                        compra[i] += 5;
-                        produtos[i] = "Pastel  - 5  Reais";
-                        break;
-                    case "coxinha":
-                        compra[i] += 4;
-                        produtos[i] = "Coxinha - 4  Reais";
-                        break;
-                    case "bolo":
-                        compra[i] += 35;
-                        produtos[i] = "Bolo    - 35 Reais";
-                        break;
-                    case "c":
-                        Console.WriteLine(compra[i]);
-                        i--;
-                        break;
-                    case "s":
+                matriz[i] = new int[3];
+            }
 
-                        break;
-                    default:
-                        i--;
-                        Console.WriteLine("Produto não encontrado");
-                        break;
+            for (int i = 0; i < matriz.Length; i++)
+            {
+                for (int j = 0; j < matriz[i].Length; j++)
+                {
+
+                    if (i < j)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        valorFinal += matriz[i][j];
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green; 
+                    }
+
+                        Console.Write(matriz[i][j] + " ");
                 }
+
+                Console.WriteLine();
 
             }
+
+            Console.WriteLine();
+            Console.WriteLine("O valor da soma acima da diagonal é: " + valorFinal);
+            
 
         }
     }
