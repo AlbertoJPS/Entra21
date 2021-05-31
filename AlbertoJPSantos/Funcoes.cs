4022e1fa09bfbc44054cccb0e0e151842864ef40
AlbertoJPS <albertojps.psn@outlook.com>
2021-05-31 12:11:04 -0300

    add

--- AlbertoJPSantos/Funcoes.cs
+++ AlbertoJPSantos/Funcoes.cs
@@ -8,52 +8,64 @@ namespace AlbertoJPSantos
 {
     public static class Funcoes
     {
-
-        public static void Entradas()
-        {
-            
-            
-        }
         public static void Matriz()
         {
-            int[][] matriz = new int[6][];
+            //gera matriz
 
-            for (int i = 0; i < matriz.Length; i++)
-            {
-                matriz[i] = new int[6];
-            }
+            // 0.0 0.1 0.2 0.3 0.4 0.5 
+            // 1.0 1.1 1.2 1.3 1.4 1.5 
+            // 2.0 2.1 2.2 2.3 2.4 2.5 
+            // 3.0 3.1 3.2 3.3 3.4 3.5 
+            // 4.0 4.1 4.2 4.3 4.4 4.5 
+            // 5.0 5.1 5.2 5.3 5.4 5.5
 
-            for (int i = 0; i < matriz.Length; i++)
+
+            ////
+            //// fazer lista de matrizes
+            //List<string[][]> listMatriz = new List<string[][]>(4);
+            ////
+
+            int id = 1;
+            int[] numMatriz = new int[4];
+
+            foreach (var estoque in numMatriz)
             {
-                for (int j = 0; j < matriz[i].Length; j++)
+                Console.WriteLine("\nEstoque " + id);
+
+                int[][] matriz = new int[6][];
+                for (int i = 0; i < matriz.Length; i++)
                 {
-                    matriz[i][j] = i + j;
+                    matriz[i] = new int[6];
                 }
-            }
-
-            for (int i = 0; i < matriz.Length; i++)
-            {
-                for (int j = 0; j < matriz[i].Length; j++)
+                for (int i = 0; i < matriz.Length; i++)
                 {
-                    if (i < 3)
+                    for (int j = 0; j < matriz[i].Length; j++)
                     {
-                        Console.Write(1);
+                        matriz[i][j] = i + j;
                     }
-                    else
-                    {
-                        Console.Write(0);
-                    }
-                    
-
-                    // 0.0 0.1 0.2 0.3 0.4 0.5 
-                    // 1.0 1.1 1.2 1.3 1.4 1.5 
-                    // 2.0 2.1 2.2 2.3 2.4 2.5 
-                    // 3.0 3.1 3.2 3.3 3.4 3.5 
-                    // 4.0 4.1 4.2 4.3 4.4 4.5 
-                    // 5.0 5.1 5.2 5.3 5.4 5.5
                 }
-                Console.WriteLine();
+
+                // escreve matriz inicial
+
+                for (int i = 0; i < matriz.Length; i++)
+                {
+                    for (int j = 0; j < matriz[i].Length; j++)
+                    {
+                        if (i < 3)
+                        {
+                            Console.Write(id);
+                        }
+                        else
+                        {
+                            Console.Write(0);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                //-------------------------------------
+                id++;
             }
+
         }
 
 
--- AlbertoJPSantos/Program.cs
+++ AlbertoJPSantos/Program.cs
@@ -10,32 +10,49 @@ namespace AlbertoJPSantos
             //Escreva seus códigos Aqui;)
             //Nome: Alberto José Pereira dos Santos
 
-            Console.WriteLine("cargas");
-            int cargas = Geradores.Qtd();
-            Console.WriteLine(cargas);
+            ///1º Recebe Carga e organiza estoque
+            ///
 
+            Console.WriteLine("Cargas");
+            int[] cargas = new int[Geradores.Qtd()];
+            Console.WriteLine(cargas.Length);
 
-            Console.WriteLine("\nentradas de produtos");
+            Console.WriteLine("\nEntradas de produtos");
             List<string> entradas = new List<string>();
-            for (int i = 0; i < cargas; i++)
+            int item = 0;
+
+            for (int i = 0; i < cargas.Length; i++)
             {
                 entradas = Geradores.GeraEntrada();
 
                 foreach (var iten in entradas)
                 {
                     Console.Write(iten);
+                    item = Convert.ToInt32(iten);
                 }
+                Console.WriteLine();
             }
             
+            ///2º Mostra estoque
+            ///
 
-            Console.WriteLine("\n\nestoque");
             Funcoes.Matriz();
 
+            ///3º Envia carga
+            ///
 
-            Console.WriteLine("\nsaida de produtos");
+            Console.WriteLine("\nSaida de produtos");
             string saida = Geradores.OrdemDeServico();
             Console.WriteLine(saida);
-            
+
+            // fazer um vetor que receberá o tamanho do int sida e obter o valor de cada posição
+
+            ///4º Mostra estoque atualizado
+            ///
+
+            Console.WriteLine("\n\nPressione 'Enter' para Prossegir...");
+            Console.ReadKey();
+
         }
     }
 }
