f66284e06863cf14fb61495faa5f5d4e2fdb1f48
AlbertoJPS <albertojps.psn@outlook.com>
2021-06-28 11:58:58 -0300

    add

--- Aula OO/Heranca/Endereco.cs
+++ Aula OO/Heranca/Endereco.cs
@@ -12,5 +12,6 @@ namespace Aula_OO.Heranca
         public string Cep { get; set; }
         public string Cidade { get; set; }
         public string Estado { get; set; }
+        public string Pais { get; set; }
     }
 }
--- Aula OO/Program.cs
+++ Aula OO/Program.cs
@@ -488,25 +488,30 @@ namespace Aula_OO
             /// Exc 01
             /// 
 
-            Empresa emp = new Empresa();
-            Console.WriteLine("Preencha os dados da empresa");
-            emp.Nome = CR();
-            emp.Idade = Convert.ToInt32(CR());
-            emp.Saldo = Convert.ToInt32(CR());
-            emp.QtdFuncionarios = Convert.ToInt32(CR());
-            emp.Cnpj = CR();
-            emp.Rua = CR();
-            emp.Cep = CR();
-            emp.Cidade = CR();
-            emp.Estado = CR();
-
-            if (emp.QtdFuncionarios > 4)
-            {
-                Console.WriteLine("{0} {1} {2}" + emp.Nome, emp.Idade, emp.Cnpj);
-                Console.WriteLine("{0:c} {1}" + emp.Saldo, emp.QtdFuncionarios);
-                Console.WriteLine("{0} {1} {2}" + emp.Rua, emp.Cep, emp.Cidade);
-                Console.WriteLine("{0} " + emp.Estado);
-            }
+            //Empresa emp = new Empresa();
+            //Console.WriteLine("Preencha a Empreza");
+            //emp.Nome = CR();
+            //emp.Idade = Convert.ToInt32(CR());
+            //emp.Saldo = Convert.ToDouble(CR());
+            //emp.QtdFuncionarios = int.Parse(CR());
+            //emp.Cnpj = CR();
+            //emp.Rua = CR();
+            //emp.Cep = CR();
+            //emp.Cidade = CR();
+            //emp.Estado = CR();
+            //emp.Pais = CR();
+            //// formatar o CNPJ @"{0:00\.000\.000\/0000\-00}"
+            //if (emp.QtdFuncionarios > 8)
+            //{
+            //    Console.WriteLine(@"{0}  {1}  {2:00\.000\.000\/0000\-00}", emp.Nome, emp.Idade, Convert.ToInt64(emp.Cnpj));
+            //    Console.WriteLine("{0:c}  {1}", emp.Saldo, emp.QtdFuncionarios);
+            //    Console.WriteLine("{0}  {1}  {2}", emp.Rua, emp.Cep, emp.Cidade);
+            //    Console.WriteLine("{0}  {1}", emp.Estado, emp.Pais);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Micro empresa");
+            //}
 
             ///Exc 02
             ///
